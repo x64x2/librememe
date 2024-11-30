677e0d60c770789df7de4287adf52ef105c3cff7
somto <81354428+x64x2@users.noreply.github.com>
2024-11-30 02:19:22 +0000

    Delete build-container.sh

--- build-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-
-podman build -t myfans:latest -f Containerfile --platform linux/amd64,linux/arm64 dist
-podman save --format docker-archive --multi-image-archive -o dist/myfans.docker myfans
